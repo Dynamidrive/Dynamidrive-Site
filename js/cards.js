ebbb8c660537783a872cb4a59af60c9ff8421eed
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2025-07-16 00:10:38 -0700

    Your mother

---
+++ js/cards.js
@@ -0,0 +1,24 @@
+const cards = {
+  ["home"]:[
+    {
+      Name:"check out roblox",
+      Desc:"",
+      Icon:"https://images.rbxcdn.com/7c5fe83dffa97250aaddd54178900ea7.png",
+      Link:"https://roblox.com/",
+    },
+
+    {
+      Name:"check out roblox 2",
+      Desc:"",
+      Icon:"https://images.rbxcdn.com/7c5fe83dffa97250aaddd54178900ea7.png",
+      Link:"https://roblox.com/",
+    },
+
+    {
+      Name:"check out roblox part 3",
+      Desc:"",
+      Icon:"https://images.rbxcdn.com/7c5fe83dffa97250aaddd54178900ea7.png",
+      Link:"https://roblox.com/",
+    }
+  ]
+};
